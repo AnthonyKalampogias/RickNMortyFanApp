0466c5c15af6316b1a933e6ecd336886aedc093e
Anthony_Kalampogias <antonykalampogias@gmail.com>
2022-08-10 00:06:01 +0300

    Added initial way to display items on screen

--- lib/Screens/Pager.dart
+++ lib/Screens/Pager.dart
@@ -14,18 +14,45 @@ class _PagerState extends State<Pager> {
   late ApiPage page;
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<ApiPage?>(
-      future: fetchPage(requestedPage),
-      builder: (context, AsyncSnapshot<ApiPage?> snapshot) {
-        if (snapshot.hasData) {
-          page = snapshot.data!;
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-
-        // By default, show a loading spinner.
-        return const CircularProgressIndicator();
-      },
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: FutureBuilder<ApiPage?>(
+          future: fetchPage(requestedPage),
+          builder: (context, AsyncSnapshot<ApiPage?> snapshot) {
+            if (snapshot.hasData) {
+              page = snapshot.data!;
+              return SingleChildScrollView(
+                child: Card(
+                  margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 25.0,
+                      backgroundImage: NetworkImage(page.results![0].image!),
+                    ),
+                    title: Text(page.results![0].name!),
+                    subtitle: Text("Gender: ${page.results![0].gender!}"),
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // By default, show a loading spinner.
+            return Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(50),
+                  child: CircularProgressIndicator(),
+                ),
+                Text("Loading, Please wait...")
+              ],
+            ));
+          },
+        ),
+      ),
     );
   }
 }
